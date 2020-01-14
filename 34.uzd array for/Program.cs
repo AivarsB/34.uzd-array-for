5b35c00b060be2abd2a21a889b8f8cd2fd70c071
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 16:50:35 +0200

    Add project files.

---
+++ 34.uzd array for/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _34.uzd_array_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
+// Definēt int tipa masīvu, kurā būs 10 elementi. 
+// Izveidot for ciklu, kurš aizpildīs šo masīvu ar skaitļiem. Masīvā ir jāieraksta for cikla mainīgā vērtība.
